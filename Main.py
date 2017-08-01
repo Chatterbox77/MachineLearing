d20a88497d738941cebf6216793d92a8f4446b6f
Chatterbox77 <an.horban@gmail.com>
2017-08-01 13:06:16 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+from Mnist import Mnist
+from Network import Network
+
+
+m = Mnist('train-images-idx3-ubyte', 'train-labels-idx1-ubyte',
+              't10k-images-idx3-ubyte', 't10k-labels-idx1-ubyte')
+net = Network([784, 30, 10])
+net.SGD(m.train_data, 30, 10, 3.0, test_data=m.test_data)
